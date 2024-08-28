19eba300ea35190ebaaee5c53ab7ee1dcb450ac9
nadeesamaraweera <nadeesamaraweera2000@gmail.com>
2024-08-28 10:55:59 +0530

    update

--- src/main/java/com/example/jwttoken/controller/AuthController.java
+++ src/main/java/com/example/jwttoken/controller/AuthController.java
@@ -11,12 +11,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
+@CrossOrigin
 @RestController
 @RequestMapping("api/v1/auth")
 public class AuthController {
